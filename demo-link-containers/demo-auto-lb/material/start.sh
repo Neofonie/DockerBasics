5b5048e2120c45232e9ed5ac5e71e40ae76bdb9b
Andre Hirsinger <andre.hirsinger@neofonie.de>
2016-04-12 19:56:53 +0200

    haproxy graceful shutdown/reload, systemd-wrapper

--- demo-link-containers/demo-auto-lb/material/start.sh
+++ demo-link-containers/demo-auto-lb/material/start.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-haproxy_template=/etc/haproxy/haproxy.cfg
+haproxy_template=/usr/local/etc/haproxy/haproxy.cfg
 haproxy_cfg=${haproxy_template}.dyn
+backend=web
+
+test -n "$COMPOSE_PROJECT" && backend=${COMPOSE_PROJECT}_$backend
 
 cp $haproxy_template $haproxy_cfg
 
-sleep 3
-
-for n in `seq 1 9`; do
-        host web_$n | grep "has address" >/dev/null 2>&1
-        if [ "$?" -eq "0" ]; then
-                echo server web_$n web_$n:1337 check >> $haproxy_cfg
-        fi
+for n in `seq 1 19`; do
+        backend_n=${backend}_$n
+        host $backend_n | grep "has address" >/dev/null 2>&1 && echo server $backend_n $backend_n:1337 check >> $haproxy_cfg
 done
 
-exec haproxy -f $haproxy_cfg
+diff -d $haproxy_template $haproxy_cfg && { echo no backend found.; exit 1; }
+
+exec /usr/local/sbin/haproxy-systemd-wrapper -p /run/haproxy.pid -f $haproxy_cfg
