05fd516c5632793458fb702f9cb54a0c5effd2f9
Andre Hirsinger <andre.hirsinger@neofonie.de>
2016-04-13 00:00:23 +0200

    reload added

--- demo-link-containers/demo-auto-lb/material/start.sh
+++ demo-link-containers/demo-auto-lb/material/start.sh
@@ -8,11 +8,23 @@ test -n "$COMPOSE_PROJECT" && backend=${COMPOSE_PROJECT}_$backend
 
 cp $haproxy_template $haproxy_cfg
 
-for n in `seq 1 19`; do
+backends=0
+for n in `seq 1 10`; do
         backend_n=${backend}_$n
-        host $backend_n | grep "has address" >/dev/null 2>&1 && echo server $backend_n $backend_n:1337 check >> $haproxy_cfg
+        host -t A -W 1 $backend_n | grep "has address" >/dev/null 2>&1
+        if [ "$?" -eq "0" ]; then
+            echo server $backend_n $backend_n:1337 check >> $haproxy_cfg
+            let backends=$backends+1
+        else
+            test $backends -gt 0 && break
+        fi
 done
 
 diff -d $haproxy_template $haproxy_cfg && { echo no backend found.; exit 1; }
 
-exec /usr/local/sbin/haproxy-systemd-wrapper -p /run/haproxy.pid -f $haproxy_cfg
+pid=`pidof haproxy-systemd-wrapper`
+if [ "$?" -eq "0" ]; then
+    kill -1 $pid
+else
+    exec /usr/local/sbin/haproxy-systemd-wrapper -p /run/haproxy.pid -f $haproxy_cfg
+fi
