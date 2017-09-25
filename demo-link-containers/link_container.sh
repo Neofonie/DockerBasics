3ad62cfd479aa8d067e34aaad07a7cb58099d848
Andre Hirsinger <andre.hirsinger@neofonie.de>
2017-09-25 18:54:52 +0200

    neuer docker hub account

--- demo-link-containers/link_container.sh
+++ demo-link-containers/link_container.sh
@@ -4,9 +4,9 @@ function commands() {
 cat << EOF
 docker ps -a 
 docker rm -v -f web01 web02 loadbalancer
-docker run -d --name=web01 neofonie/demo-server
-docker run -d --name=web02 neofonie/demo-server
-docker run -d --name=loadbalancer --link web01:backend01 --link web02:backend02 -p 42080:8080 neofonie/demo-loadbalancer
+docker run -d --name=web01 neoworkshop/demo-server
+docker run -d --name=web02 neoworkshop/demo-server
+docker run -d --name=loadbalancer --link web01:backend01 --link web02:backend02 -p 42080:8080 neoworkshop/demo-loadbalancer
 docker ps -a 
 docker exec loadbalancer bash -c "cat /etc/hosts"
 docker exec loadbalancer bash -c "ping -c 1 web02"
